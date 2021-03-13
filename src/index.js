f27dff7b2ce9258a7350b89fec1b03cf0e753ef5
MaslovEPAMRollingScopes2020 <epamrollingscopes@gmail.com>
2021-03-13 20:41:03 +0300

    init: start Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array && array.length > 0) {
+    let i;
+    let res = array[0];
+    for(i = 0; i < array.length; i++) {
+      if(res > array[i]) {
+        res = array[i];
+      }
+    }
+    return res;
+  }
+  else 
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array && array.length > 0) {
+    let i;
+    let res = array[0];
+    for(i = 0; i < array.length; i++) {
+      if(res < array[i]) {
+        res = array[i];
+      }
+    }
+    return res;
+  }
+  else 
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array && array.length > 0) {
+    let i;
+    let res = 0;
+    for(i = 0; i < array.length; i++) {
+      res += array[i];
+    }
+    return res/i;
+  }
+  else 
+    return 0;
 }
